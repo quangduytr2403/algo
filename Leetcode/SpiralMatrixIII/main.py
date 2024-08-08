7a97bed2d0d5b01c4dd002c42e799af6ed68a6d2
Trần Quang Duy <quangduytr97@gmail.com>
2024-08-08 22:54:44 +0700

    SpiralMatrixIII

---
+++ Leetcode/SpiralMatrixIII/main.py
@@ -0,0 +1,42 @@
+# This is a sample Python script.
+from typing import List
+
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+class Solution(object):
+    def spiralMatrixIII(self, rows, cols, rStart, cStart):
+        i, j = rStart, cStart
+
+        diri, dirj = 0, 1  # directions to move
+        twice = 2
+        res = []
+        moves = 1
+        next_moves = 2
+
+        while len(res) < (rows * cols):
+            if (-1 < i < rows) and (-1 < j < cols):
+                res.append([i, j])
+
+            i += diri
+            j += dirj
+            moves -= 1
+            if moves == 0:
+                diri, dirj = dirj, -diri  # 90 deg Clockwise
+                twice -= 1
+                if twice == 0:
+                    twice = 2
+                    moves = next_moves
+                    next_moves += 1
+                else:
+                    moves = next_moves - 1
+        return res
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    pass
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
