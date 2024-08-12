2701b367385a52c8cb7f6bafb3a1354898adfe75
Trần Quang Duy <quangduytr97@gmail.com>
2024-08-12 07:41:50 +0700

    KthLargestElementInAStream

---
+++ Leetcode/KthLargestElementInAStream/main.py
@@ -0,0 +1,28 @@
+# This is a sample Python script.
+from typing import List
+
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+class KthLargest:
+
+    k = None
+    nums = None
+
+    def __init__(self, k: int, nums: List[int]):
+        self.k = k
+        self.nums = nums
+
+    def add(self, val: int) -> int:
+        self.nums.append(val)
+        self.nums.sort(reverse=True)
+        return self.nums[self.k - 1]
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    pass
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
