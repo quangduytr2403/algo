d008271cbc10a8ee3768f59cb299be8dcfe3bbf6
Trần Quang Duy <quangduytr97@gmail.com>
2024-08-15 22:17:16 +0700

    LemonadeChange

---
+++ Leetcode/LemonadeChange/main.py
@@ -0,0 +1,38 @@
+# This is a sample Python script.
+from typing import List
+
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+class Solution:
+    def lemonadeChange(self, bills: List[int]) -> bool:
+        five = 0
+        ten = 0
+        for bill in bills:
+            if bill == 5:
+                five += 1
+            elif bill == 10:
+                if five == 0:
+                    return False
+                five -= 1
+                ten += 1
+            else:
+                if ten == 0:
+                    if five < 3:
+                        return False
+                    five -= 3
+                else:
+                    if five == 0:
+                        return False
+                    ten -= 1
+                    five -= 1
+
+        return True
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    pass
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
