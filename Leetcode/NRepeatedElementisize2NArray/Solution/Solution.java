9e4782347a3b1f099f3bc5f3cffe9edf848c0429
Trần Quang Duy <quangduytr97@gmail.com>
2026-01-02 21:12:47 +0700

    NRepeatedElementisize2NArray

---
+++ Leetcode/NRepeatedElementisize2NArray/Solution/Solution.java
@@ -0,0 +1,15 @@
+import java.util.HashSet;
+import java.util.Set;
+
+class Solution {
+    public int repeatedNTimes(int[] nums) {
+        Set<Integer> s = new HashSet<>();
+
+        for (int num : nums) {
+            if (s.contains(num)) return num;
+            s.add(num);
+        }
+
+        return -1;
+    }
+}
