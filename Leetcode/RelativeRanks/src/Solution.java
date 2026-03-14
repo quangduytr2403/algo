534789e1ad4e6d3e4b2025b39b13b95140bcb7fc
Trần Quang Duy <quangduytr97@gmail.com>
2026-03-14 23:00:18 +0700

    RelativeRanks

---
+++ Leetcode/RelativeRanks/src/Solution.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Solution {
+    public String[] findRelativeRanks(int[] score) {
+        Map<Integer, Integer> m = new HashMap<>();
+        for (int i = 0; i < score.length; i++) {
+            m.put(score[i], i);
+        }
+        Arrays.sort(score);
+        String[] res = new String[score.length];
+
+        for (int i = score.length - 1; i >= 0; i--) {
+            if (i == score.length - 1) {
+                res[m.get(score[i])] = "Gold Medal";
+            } else if (i == score.length - 2) {
+                res[m.get(score[i])] = "Silver Medal";
+            } else if (i == score.length - 3) {
+                res[m.get(score[i])] = "Bronze Medal";
+            } else {
+                res[m.get(score[i])] = "" + (score.length - i);
+            }
+        }
+
+        return res;
+    }
+}
