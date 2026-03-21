c69ca8f9c047a94b67799219db508b80f208bca8
Trần Quang Duy <quangduytr97@gmail.com>
2026-03-21 08:53:16 +0700

    FlipSquareSubmatrixVertically

---
+++ Leetcode/FlipSquareSubmatrixVertically/src/Solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[][] reverseSubmatrix(int[][] grid, int x, int y, int k) {
+        int tmp;
+
+        for (int i = 0; i <= (k % 2 == 0 ? k / 2 - 1 : k / 2); i++) {
+            for (int j = 0; j < k; j++) {
+                tmp = grid[x + i][y + j];
+                grid[x + i][y + j] = grid[x + k - 1 - i][y + j];
+                grid[x + k - 1 - i][y + j] = tmp;
+            }
+        }
+
+        return grid;
+    }
+}
