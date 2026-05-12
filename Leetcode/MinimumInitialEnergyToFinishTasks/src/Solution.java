cb7c5f2806d76e7e57e079379d6d8c513d71fbcc
Trần Quang Duy <quangduytr97@gmail.com>
2026-05-12 08:27:35 +0700

    MinimumInitialEnergyToFinishTasks

---
+++ Leetcode/MinimumInitialEnergyToFinishTasks/src/Solution.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+class Solution {
+    public int minimumEffort(int[][] tasks) {
+        Arrays.sort(tasks, (a, b) -> (b[1] - b[0]) - (a[1] - a[0]));
+        int currentEnergy = 0;
+        int initialEnergy = 0;
+        for (int[] task : tasks) {
+            int actual = task[0];
+            int minimum = task[1];
+            if (currentEnergy < minimum) {
+                initialEnergy += (minimum - currentEnergy);
+                currentEnergy = minimum;
+            }
+            currentEnergy -= actual;
+        }
+        return initialEnergy;
+    }
+}
